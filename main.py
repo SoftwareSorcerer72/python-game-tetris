7d2c6e249e937d59ee06669dbcb48692fee6f39b
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-20 02:59:56 -0400

    Set bottom boundary and shapes to regenerate at random on the top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from copy import deepcopy
+from random import choice, randrange
 
 W, H = 10, 20
 TILE = 45
@@ -22,14 +23,17 @@ figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
 
 figures = [[pygame.Rect(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
 figure_rect = pygame.Rect(0, 0, TILE - 2, TILE - 2)
+field = [[0 for i in range(W)] for j in range(H)]
 
 
 anim_count, anim_speed, anim_limit = 0, 60, 2000
-figure= deepcopy(figures[3])
+figure= deepcopy(choice(figures))
 
 
 def check_borders():
     if figure[i].x < 0 or figure[i].x > W - 1:
+        return False
+    elif figure [i].y > H -1 or field[figure[i].y][figure[i].x]:
         return False
     return True
 
@@ -63,7 +67,9 @@ while True:
         for i in range(4):
             figure[i].y += 1
             if not check_borders():
-                figure = deepcopy(figure_old)
+                for i in range(4):
+                    field[figure_old[i].y][figure_old[i].x] = pygame.Color('white')
+                figure = deepcopy(choice(figures))
                 anim_limit = 2000
                 break    
 
@@ -75,6 +81,12 @@ while True:
         figure_rect.x = figure[i].x * TILE
         figure_rect.y = figure[i].y * TILE
         pygame.draw.rect(game_sc, pygame.Color('white'), figure_rect)
+    #draw field
+    for y, raw in enumerate(field):
+        for x, col in enumerate(raw):
+            if col:
+                figure_rect.x, figure_rect.y = x * TILE, y * TILE
+                pygame.draw.rect(game_sc, col, figure_rect)
 
 
     pygame.display.flip()
